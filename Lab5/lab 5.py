d7cc0ed19ad2332e8cd52000b58d3fb996e42b1c
sarah19-meet <sarah19@meet.mit.edu>
2017-11-22 19:24:05 +0200

    idan help[ed me

---
+++ Lab5/lab 5.py
@@ -0,0 +1,16 @@
+from turtle import *
+colormode(255)
+class turtle(object):
+    def __init__(self, size):
+
+class square(turtle):
+    def __int__(self, size, shape)
+    turtle.__init__(self)
+    self.size(77)
+    self.shape("square")
+
+class rectangle(object):
+    def __init__(self, width, height):
+        self.width(15)
+        selft.height(6)
+            
