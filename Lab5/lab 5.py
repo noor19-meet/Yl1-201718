1c62b1b524046c6c4697fbf1cff4a4e69e0668cd
sarah19-meet <sarah19@meet.mit.edu>
2017-11-29 19:06:58 +0200

    noor

--- Lab5/lab 5.py
+++ Lab5/lab 5.py
@@ -1,16 +1,60 @@
 from turtle import *
 colormode(255)
-class turtle(object):
-    def __init__(self, size):
 
-class square(turtle):
-    def __int__(self, size, shape)
-    turtle.__init__(self)
-    self.size(77)
-    self.shape("square")
+##class Square(Turtle):
+##    def __init__(self, size):
+##        Turtle.__init__(self)
+##        self.shapesize(size)
+##        self.shape("square")
+##
+##sq  = Square(10)
 
-class rectangle(object):
+class Rectangle(Turtle):
     def __init__(self, width, height):
-        self.width(15)
-        selft.height(6)
-            
+        Turtle.__init__(self)
+        self.penup()
+        self.begin_poly()
+        self.fd(width)
+        self.left(90)
+        self.fd(height)
+        self.left(90)
+        self.fd(width)
+        self.left(90)
+        self.fd(height)
+        self.left(90)
+        self.end_poly()
+        r1 = self.get_poly()
+        register_shape("Rectangle", r1)
+        self.shape("Rectangle")
+r1 = Rectangle(200, 80)
+
+##class Square(Rectangle):
+##    def __init__(self):
+
+
+##class Hexagon(Turtle):
+##    def __init__(self, size, color, age):
+##        Turtle.__init__(self)
+##        self.penup()
+##        self.begin_poly()
+##        self.fd(size)
+##        self.left(60)
+##        self.fd(size)
+##        self.left(60)
+##        self.fd(size)
+##        self.left(60)
+##        self.fd(size)
+##        self.left(60)
+##        self.fd(size)
+##        self.left(60)
+##        self.fd(size)
+##        self.left(60)
+##        self.end_poly()
+##        h1 = self.get_poly()
+##        register_shape("Hexagon", h1)
+##        self.shapesize(size)
+##        self.shape("Hexagon")
+##        self.color(color)
+##        self.age(age)
+##h1 = Hexagon(7,"red", 8)
+##
